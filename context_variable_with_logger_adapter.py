057ca44425bd1106272d9231c8ba949dddedbe61
mijoo308 <mijoo308@gmail.com>
2021-11-03 09:53:49 +0900

    fix : using context variable

--- context_variable_with_logger_adapter.py
+++ context_variable_with_logger_adapter.py
@@ -1,17 +1,22 @@
 import uuid
 import logging
-
-request_id = uuid.uuid1()
-
-## 새로운 속성 생성 -> 기존 record 속성처럼 사용가능
-extra = {'request_id':request_id}
+from contextvars import ContextVar
+var = ContextVar('request_id')
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 syslog = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s %(request_id)s : %(message)s')
-syslog.setFormatter(formatter)
-logger.setLevel(logging.INFO)
 logger.addHandler(syslog)
 
-logger = logging.LoggerAdapter(logger, extra)
-logger.info('Heyyy')
+
+class myLogger(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        return '%s : %s' % (var.get(), msg), kwargs
+
+
+logger = myLogger(logger, {'request_id': ''})
+
+
+for i in range(3):
+    var.set(uuid.uuid1())
+    logger.info('heyyy')
