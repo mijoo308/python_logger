07db79f04def70d86f337b0a91376ea1e5d933b7
mijoo308 <mijoo308@gmail.com>
2021-11-05 15:13:02 +0900

    add : handler filter

--- config_file_test.py
+++ config_file_test.py
@@ -13,31 +13,48 @@ class myLogger(logging.LoggerAdapter):
         # return '[%s] %s' % (self.extra['connid'], msg), kwargs
         return REQUEST_ID.get() + ';' + msg, kwargs
 
-
 REQUEST_ID = ContextVar('request_id')
 REQUEST_ID.set(str(uuid.uuid1()))
 
-# new handler
+simple_formatter = logging.Formatter("[%(name)s " + REQUEST_ID.get() + " %(message)s]")
+# complex_formatter = logging.Formatter(
+#     "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] - %(message)s"
+# )
 
+
+class LevelFilter(logging.Filter):
+    def __init__(self, low, high):
+        self._low = low
+        self._high = high
+        logging.Filter.__init__(self)
+    def filter(self, record):
+        if self._low <= record.levelno <= self._high:
+            return True
+        return False
+
+# new handler
 
 class DictHandler(logging.Handler):  # Inherit from logging.Handler
     def __init__(self, log_dict):
         logging.Handler.__init__(self)
         self.log_dict = log_dict
+        self.info = ' (%s line %s)'
 
     def emit(self, record):
-        self.log_dict[REQUEST_ID.get()].append(record.msg % record.args)
+        message = record.msg % record.args + self.info %(record.filename, record.lineno)
+        self.log_dict[REQUEST_ID.get()].append(message)
 
 
-def get_child_logger(root_name, child_name):
-    return logging.getLogger(root_name).getChild(child_name)
+def get_child_logger(logger_name, child_name):
+    return logging.getLogger(logger_name).getChild(child_name)
 
 
 if __name__ == "__main__":
     # load config file
     logging.config.fileConfig("logging.conf")
-
+    logging.root.handlers[0].addFilter(LevelFilter(logging.DEBUG, logging.INFO))
     root_logger = logging.root
+    root_logger.handlers[0].setFormatter(simple_formatter)
     parent_logger = logging.getLogger('declaration')
     log_dict = defaultdict(list)
     dict_handler = DictHandler(log_dict)
@@ -53,12 +70,16 @@ if __name__ == "__main__":
     parent_logger.error("error_root")
     REQUEST_ID.set(str(uuid.uuid1()))
 
-    child_logger = get_child_logger('declaration', 'blahblah_module')
+    ''' issue : 상위 로거에 달려 있는 어댑터(myLogger)는 하위 로거에 상속 안 됨,,,  '''
+    # ->get child logger return 때 어댑터 달아주면 됨
+
+    # parent_logger.name
+
+    child_logger = get_child_logger(parent_logger.name, 'blahblah_module') #### logger.name으로 바꿔줘야 함
     child_logger.debug("debug_module")
     child_logger.info("info_module")
     child_logger.warning("warn_module")
     child_logger.error("error_module")
-    test_list = ['7', '6', '5']
-    child_logger.error('list_test %s', test_list)
+
 
     print(log_dict)
